541a4953a8b0f6231c0c46f4b3891930cda92bdc
IGragon <super1igor@gmail.com>
2023-11-04 17:41:17 +0300

    download script

--- src/data/download_data.sh
+++ src/data/download_data.sh
@@ -1,2 +1,6 @@
-!wget https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip -d ../../data/raw
-!unzip ../../data/raw/filtered_paranmt.zip -d ../../data/raw
+mkdir ../../data/interm
+mkdir ../../data/external
+mkdir ../../data/raw
+
+wget https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip -d ../../data/raw
+unzip ../../data/raw/filtered_paranmt.zip -d ../../data/raw
