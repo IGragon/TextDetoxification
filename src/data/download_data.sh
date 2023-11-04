a0603b4ec17e2c78300d08896b5610de92df9a3a
IGragon <super1igor@gmail.com>
2023-11-05 00:35:39 +0300

    fix download_data.sh

--- src/data/download_data.sh
+++ src/data/download_data.sh
@@ -5,4 +5,4 @@ mkdir ../../data/raw
 wget -O ../../data/raw/filtered_paranmt.zip  https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip
 unzip ../../data/raw/filtered_paranmt.zip -d ../../data/raw
 
-wget -O ../../data/external/toxicity_en.csv https://github.com/surge-ai/toxicity/blob/main/toxicity_en.csv
+wget -O ../../data/external/toxicity_en.csv https://github.com/surge-ai/toxicity/raw/main/toxicity_en.csv
